090877acc2d733e79e63414fc11eb310ab85bf9d
Guido Sales Calvano <guidocalvano@yahoo.com>
2016-04-01 01:02:49 +0200

    campfire

--- startFire.js
+++ startFire.js
@@ -2,6 +2,6 @@ FireControl = require('./FireControl');
 
 var width     = 6;
 var height    = 8;
-var frameRate = 8.5;
+var frameRate = 5;
 var skipFirstNPixels = 2;
 var fc = new FireControl().init(width, height, frameRate, skipFirstNPixels);
